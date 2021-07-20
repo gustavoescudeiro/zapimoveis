17321757fb7eff225987ea768b9c2ed1f962c502
gustavo_escudeiro <gustavo.escudeiro@hotmail.com>
2021-07-20 19:34:00 -0300

    adicionando csv

--- zap_scrap.py
+++ zap_scrap.py
@@ -3,6 +3,8 @@ import numpy as np
 import requests
 from bs4 import BeautifulSoup
 import re
+import time
+import datetime
 
 list_dataframezao = []
 headers = {'User-Agent': 'Mozilla/5.0'}
@@ -11,12 +13,14 @@ headers = {'User-Agent': 'Mozilla/5.0'}
 #headers.update({
 #    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
 #})
-for pg in range(1,3):
-    print(pg)
+ini_contagem = 0
+for pg in range(1,546):
+    print("pagina",pg)
     url = f'https://www.zapimoveis.com.br/venda/imoveis/sp+sao-paulo/?onde=,S%C3%A3o%20Paulo,S%C3%A3o%20Paulo,,,,,city,BR%3ESao%20Paulo%3ENULL%3ESao%20Paulo,-22.909938,-47.062633&pagina={pg}&tipo=Im%C3%B3vel%20usado&transacao=Venda'
     r = requests.get(url, headers=headers).text
     soup = BeautifulSoup(r, 'html.parser')
     imoveis = soup.find_all('div', class_=['box--display-flex'])
+
 
     list_imoveis = []
     for i in imoveis:
@@ -33,7 +37,7 @@ for pg in range(1,3):
         try:
             endereco = i.find('p', class_=['color-dark text-regular simple-card__address']).get_text()
             print(endereco)
-            dict_infos['endereco'] = endereco.rstrip().replace(" ", "")
+            dict_infos['endereco'] = endereco.rstrip()
         except:
             print(None)
         metragem = i.find('div', class_=['simple-card__actions'])
@@ -90,7 +94,19 @@ for pg in range(1,3):
         df_imovel = pd.DataFrame([dict_infos])
         list_imoveis.append(df_imovel)
 
-    dataframezao = pd.concat(list_imoveis, axis = 0)
-    list_dataframezao.append(dataframezao)
+    try:
+        dataframezao = pd.concat(list_imoveis, axis = 0)
+        list_dataframezao.append(dataframezao)
+    except:
+        print('Empty')
 
+    ini_contagem += 1
+    if ini_contagem % 250 == 0:
+        time.sleep(60*10)
+
+today = date.today()
+hoje = today.strftime("%Y-%m-%d")
+dataframe_final = pd.concat(list_dataframezao, axis = 0)
+dataframe_final['data'] = hoje
+dataframe_final.to_csv('zapimoveis.csv', sep = ';', decimal = ',', index = False)
 print('a')
